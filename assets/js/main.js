3d470227fa22cca1cb709ae19f070d0e1496dfdc
NikolaBkic27 <stuff2716@gmail.com>
2022-12-14 13:04:26 +0100

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 window.onload = function(){
     let url = document.location.pathname;
     console.log(url);
-    if(url == "/" || url == "/index.html" || url == "http://127.0.0.1:5500/" || url="nikolabkic27.github.io/"){
+    if(url == "/" || url == "/index.html" || url == "http://127.0.0.1:5500/" || url =="nikolabkic27.github.io/"){
         let sectionLocation = document.querySelector("#service");
 //Start of Pre-filter content
 let movieText = [`Legendary marshal Wyatt Earp, now a weary gunfighter, joins his brothers Morgan and Virgil to pursue their collective fortune in the thriving mining town of Tombstone. But Earp is forced to don a badge again and get help from 				

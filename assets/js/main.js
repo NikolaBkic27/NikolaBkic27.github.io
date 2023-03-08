a0bde06e338df40be348db21dbaca8a8c1c64f06
NikolaBkic27 <stuff2716@gmail.com>
2023-03-08 08:38:45 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //https://www.free-css.com/free-css-templates/page282/leadmark
 if(window.location.href == "https://nikolabkic27.github.io/"){
-    console.log(USPEH);
+    console.log("USPEH");
 }
 let navigationLocation = document.querySelector("#nb-navigation");
 let navigationDefault = `<div class="container">
